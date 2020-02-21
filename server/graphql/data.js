a5ff80148d600489d75676f57aec12af6c7b1ae7
helloitsjoe <jboyle617@gmail.com>
2020-02-21 06:45:58 -0500

    Move db into separate file

--- server/graphql/data.js
+++ server/graphql/data.js
@@ -1,67 +1,18 @@
+const data = require('./mockData');
 const { matchName } = require('../utils');
-
-const MOVIE = {
-  RAIDERS: 'Raiders of the Lost Ark',
-  TEMPLE: 'Temple of Doom',
-  CRUSADE: 'The Last Crusade',
-  INCREDIBLES: 'The Incredibles',
-  BATMAN: 'Batman',
-  X_MEN: 'X-Men',
-};
-
-const { RAIDERS, TEMPLE, CRUSADE, INCREDIBLES, BATMAN, X_MEN } = MOVIE;
-
-const heroes = [
-  { name: 'Mr. Incredible', powers: ['strength', 'invulnerability'], movies: [INCREDIBLES] },
-  { name: 'Mrs. Incredible', powers: ['stretch', 'strength'], movies: [INCREDIBLES] },
-  { name: 'Dash', powers: ['speed'], movies: [INCREDIBLES] },
-  { name: 'Violet', powers: ['invisibility'], movies: [INCREDIBLES] },
-  {
-    name: 'Jack-Jack',
-    powers: ['fire', 'transformation', 'teleportation', 'strength', 'stretch'],
-    movies: [INCREDIBLES],
-  },
-  {
-    name: 'Indiana Jones',
-    powers: ['whip', 'intelligence'],
-    movies: [RAIDERS, TEMPLE, CRUSADE],
-  },
-  { name: 'Batman', powers: ['technology'], movies: [BATMAN] },
-  { name: 'Wolverine', powers: ['adamantium', 'healing'], movies: [X_MEN] },
-];
-
-const villains = [
-  { name: 'Bane', powers: ['strength', 'invulnerability'], movies: [BATMAN] },
-  { name: 'The Joker', powers: ['psychology', 'chaos'], movies: [BATMAN] },
-  { name: 'Magneto', powers: ['magnetism'], movies: [X_MEN] },
-  { name: 'Syndrome', powers: ['technology'], movies: [INCREDIBLES] },
-  { name: 'Nazis', powers: ['facism'], movies: [RAIDERS, CRUSADE] },
-  { name: 'Mola Ram', powers: ['dark magic', 'brainwashing'], movies: [TEMPLE] },
-];
-
-const createMovie = name => {
-  const isInMovie = character => character.movies.includes(name);
-  return {
-    name,
-    heroes: heroes.filter(isInMovie).map(h => h.name),
-    villains: villains.filter(isInMovie).map(v => v.name),
-  };
-};
-
-const movies = Object.values(MOVIE).map(createMovie);
 
 const wait = (ms = 250) => new Promise(resolve => setTimeout(resolve, ms));
 
 const fetchHeroes = (name, power) => {
   const byName = h => !name || matchName(h, name);
   const byPower = h => !power || h.powers.includes(power);
-  return wait().then(() => heroes.filter(byName).filter(byPower));
+  return wait().then(() => data.heroes.filter(byName).filter(byPower));
 };
 
 const fetchVillains = (name, power) => {
   const byName = h => !name || matchName(h, name);
   const byPower = h => !power || h.powers.includes(power);
-  return wait().then(() => villains.filter(byName).filter(byPower));
+  return wait().then(() => data.villains.filter(byName).filter(byPower));
 };
 
 const fetchMovies = (name, castMemberName) => {
@@ -69,13 +20,11 @@ const fetchMovies = (name, castMemberName) => {
   const byCast = m =>
     !castMemberName ||
     m.heroes.concat(m.villains).some(n => matchName({ name: n }, castMemberName));
-  return wait().then(() => movies.filter(byName).filter(byCast));
+  return wait().then(() => data.movies.filter(byName).filter(byCast));
 };
 
 module.exports = {
-  // heroes,
   fetchHeroes,
-  fetchMovies,
   fetchVillains,
-  // movies,
+  fetchMovies,
 };
---
+++ server/graphql/mockData.js
@@ -0,0 +1,55 @@
+const MOVIE = {
+  RAIDERS: 'Raiders of the Lost Ark',
+  TEMPLE: 'Temple of Doom',
+  CRUSADE: 'The Last Crusade',
+  INCREDIBLES: 'The Incredibles',
+  BATMAN: 'Batman',
+  X_MEN: 'X-Men',
+};
+
+const { RAIDERS, TEMPLE, CRUSADE, INCREDIBLES, BATMAN, X_MEN } = MOVIE;
+
+const heroes = [
+  { name: 'Mr. Incredible', powers: ['strength', 'invulnerability'], movies: [INCREDIBLES] },
+  { name: 'Mrs. Incredible', powers: ['stretch', 'strength'], movies: [INCREDIBLES] },
+  { name: 'Dash', powers: ['speed'], movies: [INCREDIBLES] },
+  { name: 'Violet', powers: ['invisibility'], movies: [INCREDIBLES] },
+  {
+    name: 'Jack-Jack',
+    powers: ['fire', 'transformation', 'teleportation', 'strength', 'stretch'],
+    movies: [INCREDIBLES],
+  },
+  {
+    name: 'Indiana Jones',
+    powers: ['whip', 'intelligence'],
+    movies: [RAIDERS, TEMPLE, CRUSADE],
+  },
+  { name: 'Batman', powers: ['technology'], movies: [BATMAN] },
+  { name: 'Wolverine', powers: ['adamantium', 'healing'], movies: [X_MEN] },
+];
+
+const villains = [
+  { name: 'Bane', powers: ['strength', 'invulnerability'], movies: [BATMAN] },
+  { name: 'The Joker', powers: ['psychology', 'chaos'], movies: [BATMAN] },
+  { name: 'Magneto', powers: ['magnetism'], movies: [X_MEN] },
+  { name: 'Syndrome', powers: ['technology'], movies: [INCREDIBLES] },
+  { name: 'Nazis', powers: ['facism'], movies: [RAIDERS, CRUSADE] },
+  { name: 'Mola Ram', powers: ['dark magic', 'brainwashing'], movies: [TEMPLE] },
+];
+
+const createMovie = name => {
+  const isInMovie = character => character.movies.includes(name);
+  return {
+    name,
+    heroes: heroes.filter(isInMovie).map(h => h.name),
+    villains: villains.filter(isInMovie).map(v => v.name),
+  };
+};
+
+const movies = Object.values(MOVIE).map(createMovie);
+
+module.exports = {
+  heroes,
+  villains,
+  movies,
+};
