6d8a23b2b9e3d07ad6b4f15e7dd2543c76a7ad2e
helloitsjoe <jboyle617@gmail.com>
2020-03-25 07:50:08 -0400

    revert to my eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   // extends: ['preact', 'helloitsjoe'],
-  extends: 'preact',
+  // extends: 'preact',
+  extends: 'helloitsjoe',
   rules: {
     'react/jsx-one-expression-per-line': 'off',
   },
